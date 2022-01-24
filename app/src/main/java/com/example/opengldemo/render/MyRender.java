40c5a27919888fc7ae89a204a4f14969f3bc3a78
chunjiang1 <chunjiang1@staff.weibo.com>
2022-01-24 16:04:32 +0800

    create

---
+++ app/src/main/java/com/example/opengldemo/render/MyRender.java
@@ -0,0 +1,32 @@
+package com.example.opengldemo.render;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by chunjiang1 on 2022/1/24.
+ */
+public class MyRender implements GLSurfaceView.Renderer {
+
+    public MyRender(Context context) {
+
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+
+    }
+}
